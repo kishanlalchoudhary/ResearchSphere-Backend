0efec6629f4ab4d21b1ee568e2e81059b27a7b3c
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2024-07-24 08:32:06 +0530

    fix: cors options added

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const app = express();
 connectDB();
 
-app.use(
-  cors({
-    origin: config.frontendUrl,
-    credentials: true,
-  })
-);
+const whitelist = [config.frontendUrl];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
